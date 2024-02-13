5da0619401fcacab1fea20f7591e937e8a503dc0
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-02-13 07:41:44 +0545

    Revert "added dummy UI"

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 const port = 3000
 
-app.get('/home',(req, res)=>{
+app.get('/',(req, res)=>{
     res.send("Hello World")
 })
-// Serve static files from the 'frontend' directory
-app.use('/', express.static(path.join(__dirname, '../frontend')));
-
-// Route for rendering index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-});
 app.listen(port,()=>{
     console.log(`listening to port ${port}`)
 })
