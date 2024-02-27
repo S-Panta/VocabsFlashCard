b383ad71fa82dfc47f1edf2c2a155b55bece87dd
Sabin <sabinpanta11@gmail.com>
2024-02-27 07:37:02 +0545

    added user-auth route and controller

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 3000
+
+//using the routes
+const authRoute = require('./routes/authRoute')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.json());
 
 app.get('/home',(req, res)=>{
     res.send("Hello World")
 })
-// Serve static files from the 'frontend' directory
-app.use('/', express.static(path.join(__dirname, '../frontend')));
+// const dbURI = 'mongodb://127.0.0.1:27017/myapp';
+// mongoose.connect(dbURI)
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
 
-// Route for rendering index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-});
+// // routes
+app.use(authRoute)
+
 app.listen(port,()=>{
-    console.log(`listening to port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String, 
+        lowercase: true, 
+        required: true, 
+        match: [/\S+@\S+\.\S+/, 'is invalid'], 
+        index: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+      }
+})
+
+const User  = new mongoose.model('user',userSchema)
+module.exports = User
