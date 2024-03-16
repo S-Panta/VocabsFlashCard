36f2bf0837f70bcc9b6ab2d7f57094e2da27fd7d
Sabin <sabinpanta11@gmail.com>
2024-03-16 21:10:15 +0545

    added script for uploading file to database and defined database schema

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const authRoute = require('./routes/authRoute')
 const swaggerUi = require('swagger-ui-express')
 const specs = require('./swagger')
-const cors = require('cors')
+
 require('dotenv').config()
 
 const port = process.env.PORT
 const dbURI = process.env.dbURI
 
 const app = express()
+// series of middleware used in this app
+app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use(express.json())
 app.use(authRoute)
-app.use(cors())
 
 mongoose.connect(dbURI)
   .then(() => {
