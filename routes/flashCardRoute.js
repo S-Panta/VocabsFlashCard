c4e55b06d65d6d24a0482688275e63ddb03f0ee8
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-04-04 09:13:53 +0545

    [test-pr]test pr for adding SSR (#55)
    * test pr for adding SSR
    
    * folder restructuring

---
+++ routes/flashCardRoute.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const router = Router()
+const { authenticateMiddleware, checkAdminMiddleWare } = require('../middleware/authMiddleware')
+const { updateStatusToFlashCard, getFlashCard, getAllFlashCards } = require('../controllers/flashCardController')
+
+// middleware removed just to test ssr
+router.get('/api/flashcards/:id', getFlashCard)
+// router.get('/api/flashcards/:id', authenticateMiddleware, getFlashCard)
+router.get('/api/flashcards', authenticateMiddleware, getAllFlashCards)
+router.put('/api/flashcards/:id/update', authenticateMiddleware, updateStatusToFlashCard)
+
+// flashcard routes for admin user
+// create new flashcard, delete flashcard, update flashcard, get
+router.get('/api/admin/flashcards', authenticateMiddleware, checkAdminMiddleWare, getAllFlashCards)
+// router.post('/api/admin/')
+
+module.exports = router
