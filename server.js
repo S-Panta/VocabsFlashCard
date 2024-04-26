af9556b1760d0a6f1c73ee2d4c7dd5928b3633bb
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-04-26 07:13:46 +0545

    deleted all ejs file (#72) (#73)

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ require('dotenv').config()
 const port = process.env.PORT
 const dbURI = process.env.dbURI
 
+// Create GET request
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
+  
 mongoose.connect(dbURI)
     .then(() => {
         console.log('Database connected')
